4b6adeda7be36fbe7edcf4b4e9e74fe9a4ff76c8
pgeeser <pgeeser@student.42heilbronn.de>
2023-05-09 19:08:49 +0200

    changes

--- srcs/requirements/mariadb/prerequisites/start-script.sh
+++ srcs/requirements/mariadb/prerequisites/start-script.sh
@@ -15,7 +15,7 @@ if grep -Fxq "bind-address            = 127.0.0.1" "/etc/mysql/mariadb.conf.d/50
 	# ---------------------------- SET MYSQL SETTINGS ---------------------------- #
 	service mysql start
 	sleep 1
-	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;"
+	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;"
 	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USERNAME'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'pgeeser'@'%';"
